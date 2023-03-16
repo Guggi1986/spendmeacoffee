ec660234a889c0880f1d398b1671c680d53e6238
Guggi1986 <62639461+Guggi1986@users.noreply.github.com>
2023-03-16 10:54:40 +0100

    10:54

--- js/colors.js
+++ js/colors.js
@@ -4,10 +4,15 @@ const themes = {
   contentBackgroundColor: "#0D1117",
 
   textColor: "#ffffff",
-  linkColor: "#28a745",
+  textColorOnHover: "#28a745",
+  
+  linkColor: "#ffffff",
+  linkColorOnHover: "#28a745",
 
   button: "#238636",
-  buttonhover: "#28a745",
+  buttonHover: "#28a745",
+
+  hamburgerMenuBackgroundColor: "#010409",
 };
 
 // Define CSS variables for the entire document
@@ -19,9 +24,15 @@ root.style.setProperty("--body-Background-Color", themes.bodyBackgroundColor);
 root.style.setProperty("--content-Background-Color", themes.contentBackgroundColor);
 
 root.style.setProperty("--text-color", themes.textColor);
+root.style.setProperty("--text-color-on-Hover", themes.textColorOnHover);
+
+
 root.style.setProperty("--link-color", themes.linkColor);
+root.style.setProperty("--link-color-on-hover", themes.linkColorOnHover);
 
 root.style.setProperty("--button", themes.button);
-root.style.setProperty("--buttonhover", themes.buttonhover);
+root.style.setProperty("--buttonhover", themes.buttonHover);
+
+root.style.setProperty("--hamburger-menu-background-color", themes.hamburgerMenuBackgroundColor);
 
 
